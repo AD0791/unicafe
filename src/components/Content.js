c96722af9a43d2eb5b96943b221edc5ec6468b24
AD0791 <alexdisla1991@gmail.com>
2022-01-19 10:01:53 -0500

    1.5 exercise done

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Part from '../common/Part'
 
-function Content({part, exercises}) {
+function Content({course}) {
+    const part1 = course.parts[0]
+    const part2 = course.parts[1]
+    const part3 = course.parts[2]
     return (
         <div>
-            <Part part={part} exercises={exercises}/>
+            <Part part={part1.name} exercises={part1.exercises}/>
+            <Part part={part2.name} exercises={part2.exercises}/>
+            <Part part={part3.name} exercises={part3.exercises}/>
         </div>
     );
 }
--- src/components/Total.js
+++ src/components/Total.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
-function Total({exercises1, exercises2,exercises3}) {
+function Total({course}) {
+    const part1 = course.parts[0]
+    const part2 = course.parts[1]
+    const part3 = course.parts[2]
     return (
-        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+        <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
     );
 }
 
